beadb1a939c9a37c899f78f674f3201b53533198
Alexey Lavrenuke <direvius@gmail.com>
2013-04-21 16:42:14 +0400

    add dialog manager

---
+++ dialog_manager/dm.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import yaml
+from copy import deepcopy
+from datetime import datetime
+import sys
+from pprint import pprint
+
+
+def pretty_print(object):
+    class decoder_stream:
+        def write(self, s):
+            if s.startswith("'") and s.endswith("'"):
+                s = "'%s'" % s[1:-1].decode('string_escape')
+            elif s.startswith("u'") and s.endswith("'"):
+                s = "u'%s'" % s[2:-1].decode('unicode_escape').encode('utf8')
+            sys.stdout.write(s)
+
+    pprint(object, decoder_stream())
+
+
+def log(comment, object):
+    print comment
+    pretty_print(object)
+    print
+
+
+# начальное состояние long-term memory, шаблоны команд и концептов
+LTM = '''
+commands:
+  go:
+    dst: 
+    src:
+    via: ''
+  ScheduleMeeting:
+    room:
+    time:
+    duration:
+concepts:
+  - address
+'''
+
+DICTIONARY = {
+    'dst': 'место назначения',
+    'src': 'исходную точку',
+    'via': 'пункты следования',
+    'room': 'комнату',
+    'time': 'время',
+    'duration': 'продолжительность',
+}
+
+def aggregate_facts(facts):
+    result = {}
+    for (key, data) in facts:
+        if not key in result:
+            result[key] = []
+        result[key].append(data)
+    for key in result:
+        if len(result[key]) == 1:
+            result[key] = result[key][0]
+    return result
+
+
+class DM(object):
+    def __init__(self):
+        # short-term memory, держит текущий стек контекстов
+        self.stm = []
+
+        # long-term memory, держит объекты из предыдущих контекстов
+        self.ltm = yaml.load(LTM)
+        log("Загрузили LTM", self.ltm)
+
+    def supplement_context(self, facts):
+        # аггрегируем факты с одинаковыми ключами
+        facts = aggregate_facts(facts)
+
+        # если поступила команда, переключаем контекст
+        cmd = facts.pop('cmd', None)
+        if cmd:
+            if cmd in self.ltm['commands']:
+                template = deepcopy(self.ltm['commands'][cmd])
+                template['cmd'] = cmd
+                log("Нашли шаблон команды", template)
+                self.stm.append(template)
+                log("Переключили контекст", self.stm)
+
+        # дополняем фактами текущий контекст
+        context = self.stm[-1]
+        log("Текущий контекст", context)
+        for concept in context:
+            if concept in facts:
+                context[concept] = facts[concept]
+
+        log("Дополнили фактами", self.stm)
+
+    def execute(self):
+        if len(self.stm):
+            context = self.stm[-1]
+            log("Выполняем в контексте", context)
+
+            # дополняем незаполненные поля
+            for c_key in context:
+                concept = context[c_key]
+                if concept is None:
+                    concept = self.try_guess(c_key)
+                    if concept is None:
+                        return {'ask': c_key}
+                    else:
+                        context[c_key] = concept
+            return {'run': context}
+        else:
+            return {'ask': 'cmd'}
+
+    def try_guess(self, c_key):
+        result = None
+        if c_key == 'time':
+            result = datetime.now()
+        print "Guessing '%s': %s" % (c_key, result)
+        return result
+
+
+def generate_phrase(cmd):
+    phrase = []
+    if 'run' in cmd:
+        phrase.append("Запускаем:")
+        for key in cmd['run']:
+            phrase.append("%s(%s)" % (key, cmd['run'][key]))
+    if 'ask' in cmd:
+        phrase.append("Укажите")
+        key = cmd['ask']
+        if key in DICTIONARY:
+            phrase.append(DICTIONARY[key])
+
+    return ' '.join(phrase)
+
+
+def main():
+    dm = DM()
+    dm.supplement_context([
+        ('cmd', 'go'),
+        ('dst', 'велотрек в крылатском'),
+    ])
+    result = generate_phrase(dm.execute())
+    log("Результат выполнения", result)
+
+    dm.supplement_context([
+        ('src', 'Льва Толстого, 16'),
+    ])
+    result = generate_phrase(dm.execute())
+    log("Результат выполнения", result)
+
+    dm.supplement_context([
+        ('cmd', 'ScheduleMeeting'),
+        ('room', '404'),
+    ])
+    result = generate_phrase(dm.execute())
+    log("Результат выполнения", result)
+
+    dm.supplement_context([
+        ('duration', '1 час'),
+    ])
+    result = generate_phrase(dm.execute())
+    log("Результат выполнения", result)
+
+
+if __name__ == '__main__':
+    main()
