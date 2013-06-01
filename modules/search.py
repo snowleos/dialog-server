967d961ee74ffc370b411c0427f1da6d22a67489
Ilya <imeln@yandex-team.ru>
2013-06-01 19:44:57 +0400

    BROKEN: exception handling implemented. But ya api works inproperly

--- modules/search.py
+++ modules/search.py
@@ -7,15 +7,63 @@ from HTMLParser import HTMLParser
 from lxml import etree
 from pyquery import PyQuery as pq
 
-def search(term):
-    query_url = "http://xmlsearch.yandex.ru/xmlsearch?user=direvius&key=03.111162353:036864b8fd36c9f5203989ac2fdb6ac4&query=%s" % term
-    response = urllib.urlopen(query_url).read()
-    root = etree.fromstring(response)
-    #doc_tree = urllib.urlopen(query_url.encode('utf-8')).read()
-    return root.xpath('//doc/title')
+DEBUG = True
 
-try:
-    doc_title = search(' '.join(sys.argv))[0]
-    print HTMLParser().unescape(re.sub("(<[^<>]*>)", "", etree.tostring(doc_title)))
-except:
-    sys.exit(1)
+def SafeExec(func):
+    try:
+        func()
+    except Exception as inst:
+        print >> sys.stderr, "FUNC", func
+        print >> sys.stderr, "TYPE", type(inst)
+        print >> sys.stderr, "INST", inst
+        print "Не удалось найти. Попробуйте переформулировать:)"
+        sys.exit(1)
+def PrintDebug(obj):
+    if DEBUG == True:
+        print >> sys.stderr, obj
+
+class TSearchGetResult:
+    def __init__(self):
+        self.term = None
+        self.query_url = None
+        self.response = None
+        self.root = None
+        self.doc_title = None
+        self.parsed_doc = None
+
+    def Do_term(self):
+        self.term = ""
+        for line in sys.stdin:
+            self.term += line[:-1] + " "
+        PrintDebug(self.term)
+    def Do_query_url(self):
+        self.query_url = "http://xmlsearch.yandex.ru/xmlsearch?user=direvius&key=03.111162353:036864b8fd36c9f5203989ac2fdb6ac4&query=%s" % self.term
+        PrintDebug(self.query_url)
+
+    def Do_response(self):
+        self.response = urllib.urlopen(self.query_url).read()
+        PrintDebug(self.response)
+
+    def Do_root(self):
+        self.root = etree.fromstring(self.response)
+        PrintDebug(self.root)
+
+    def Do_doc_title(self):
+        doc_title = self.root.xpath('//doc/title')
+        PrintDebug(self.doc_title)
+
+    def Do_parsed_doc(self):
+        self.parsed_doc = HTMLParser().unescape(re.sub("(<[^<>]*>)", "", etree.tostring(self.doc_title)))
+        PrintDebug(self.parsed_doc)
+
+    def search(self):
+        SafeExec(self.Do_term)
+        SafeExec(self.Do_query_url)
+        SafeExec(self.Do_response)
+        SafeExec(self.Do_root)
+        SafeExec(self.Do_doc_title)
+        SafeExec(self.Do_parsed_doc)
+
+
+SearchGetResult = TSearchGetResult()
+print SearchGetResult.search().encode('utf-8')
